269ecc23777729f7c1c0a8d1d813514059ef3bd4
alepani37 <101183237+alepani37@users.noreply.github.com>
2024-10-07 11:42:01 +0200

    Update elaborazione_dati.py

--- elaborazione_dati.py
+++ elaborazione_dati.py
@@ -6,31 +6,37 @@ import pandas as pd
 import numpy as np
 
 
-dir_path = "C:\\Users\\aless\OneDrive - Università di Cagliari\\ML\\dataset tesi\\dataset_ruota\\dataset_elaborati_ruota_standard"
+dir_path = "path dati"
 df = pd.DataFrame()
 
 
 count = 1
 
+#pre elaborazione
 for path in os.scandir(dir_path):
 
     df = pd.read_csv(path)
 
     #eseguo la pre elaborazione
-    #dataset_preelaborato = preElaborationRuota(df, normalize_method= "l2")
-
-    #dataset_preelaborato.dropna(inplace=True)
-    #creazione feature vector
-    feature_vector = fv_gen_regression(df,sw=0.8)
-
-    #cancello le righe con la label 0
-    #feature_vector.drop(feature_vector[feature_vector[140] == 0].index,inplace=True)
-    feature_vector.to_csv("C:\\Users\\aless\\OneDrive - Università di Cagliari\ML\\feature vector\\fv_ruota\\fv_08s_standard\\fv"+str(count)+'.csv', index = False) 
-
-    #dataset_preelaborato.to_csv("C:\\Users\\aless\\OneDrive - Università di Cagliari\\ML\\dataset tesi\\dataset_ruota\\dataset_elaborati_ruota_l2\\"+str(count)+".csv",index = False)
+    dataset_preelaborato = preElaborationRuota(df, normalize_method= "l2")
+    dataset_preelaborato.dropna(inplace=True)
+    dataset_preelaborato.to_csv("path dati elaborati"+str(count)+".csv",index = False)
 
     count += 1
 
+count = 1
+
+dir_path = "path dati elaborati"
+for path in os.scandir(dir_path):
+
+    df = pd.read_csv(path)
+    feature_vector = fv_gen_regression(df,sw=0.8)
+
+    #cancello le righe con la label 0
+    feature_vector.drop(feature_vector[feature_vector[140] == 0].index,inplace=True)
+    feature_vector.to_csv("path salvataggio fv"+str(count)+'.csv', index = False) 
+
+    count += 1
 
 
 
